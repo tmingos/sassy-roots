f7ebe60d70fa564f1758d925307e5da553242d06
Jared Hughes <jared@anchorinnovative.com>
2013-03-04 09:49:52 -0600

    Fixed path to Modernizr; removed pre-processed -min.js files (should be concatenated & compiled locally)

--- templates/head.php
+++ templates/head.php
@@ -8,7 +8,7 @@
   <title><?php wp_title('|', true, 'right'); bloginfo('name'); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <script src="<?php echo get_template_directory_uri(); ?>/assets/js/vendor/modernizr-2.6.2.min.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/vendor/modernizr-2.6.2.min.js"></script>
 
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery-1.9.0.min.js"><\/script>')</script>
