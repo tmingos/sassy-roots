0fd9017dfbddd9a4c3552cd27797d82f63f717b0
Jared Hughes <jared@anchorinnovative.com>
2013-06-24 11:59:47 -0500

    Upgrade scss to Bootstrap v2.3.2; Upgrade Font Awesome to v3.2.1; Added front-page.php

---
+++ front-page.php
@@ -0,0 +1,10 @@
+<div class="page-header">
+  <h1>
+    <?php echo roots_title(); ?>
+  </h1>
+</div>
+
+<?php while (have_posts()) : the_post(); ?>
+  <?php the_content(); ?>
+  <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
+<?php endwhile; ?>
