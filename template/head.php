d89e16b449fb6f06d73430fb1ad1e5d5eb5635f9
Jared Hughes <jared@anchorinnovative.com>
2013-06-26 13:01:08 -0500

    Favicon meta

--- template/head.php
+++ template/head.php
@@ -6,10 +6,9 @@
 <head>
 
   <meta charset="utf-8">
-
   <title><?php wp_title('|', true, 'right'); bloginfo('name'); ?></title>
-
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" type="image/x-icon" href="/favicon.ico" />
 
   <?php wp_head(); ?>
 
