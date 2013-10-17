082e652a2a460caf1d22575572f6d524e1e096ec
Taylor Mingos <taylormingos@gmail.com>
2013-10-16 22:21:22 -0400

    Refactor Grunt build to support Sass instead of LESS

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,74 +1,114 @@
-/*
- * grunt-contrib-sass
- * http://gruntjs.com/
- *
- * Copyright (c) 2012 Sindre Sorhus, contributors
- * Licensed under the MIT license.
- */
 'use strict';
+module.exports = function(grunt) {
 
-module.exports = function (grunt) {
   grunt.initConfig({
-    pkg: {
-      name: 'grunt-contrib-sass'
-    },
     jshint: {
       options: {
         jshintrc: '.jshintrc'
       },
       all: [
         'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>'
+        'assets/js/*.js',
+        'assets/js/plugins/*.js',
+        '!assets/js/scripts.min.js'
       ]
-    },
-    clean: {
-      test: [
-        'test/tmp',
-        '.sass-cache'
-      ]
-    },
-    nodeunit: {
-      tests: ['test/*_test.js']
     },
     sass: {
-      options: {
-        //debugInfo: true,
-       // style: 'expanded'
-      },
-      compile: {
-        files: {
-          'test/tmp/scss.css': ['test/fixtures/compile.scss'],
-          'test/tmp/sass.css': ['test/fixtures/compile.sass'],
-          'test/tmp/css.css': ['test/fixtures/compile.css']
-        }
-      },
-      compileBanner: {
+      dist: {
         options: {
-          banner: '/* <%= pkg.name %> banner */'
+          style: 'compressed',
         },
         files: {
-          'test/tmp/scss-banner.css': ['test/fixtures/banner.scss'],
-          'test/tmp/sass-banner.css': ['test/fixtures/banner.sass'],
-          'test/tmp/css-banner.css': ['test/fixtures/banner.css']
+          'assets/css/main.min.css': [
+            'assets/sass/app.scss'
+          ]
         }
       }
+    },
+    uglify: {
+      dist: {
+        files: {
+          'assets/js/scripts.min.js': [
+            'assets/js/plugins/bootstrap/transition.js',
+            'assets/js/plugins/bootstrap/alert.js',
+            'assets/js/plugins/bootstrap/button.js',
+            'assets/js/plugins/bootstrap/carousel.js',
+            'assets/js/plugins/bootstrap/collapse.js',
+            'assets/js/plugins/bootstrap/dropdown.js',
+            'assets/js/plugins/bootstrap/modal.js',
+            'assets/js/plugins/bootstrap/tooltip.js',
+            'assets/js/plugins/bootstrap/popover.js',
+            'assets/js/plugins/bootstrap/scrollspy.js',
+            'assets/js/plugins/bootstrap/tab.js',
+            'assets/js/plugins/bootstrap/affix.js',
+            'assets/js/plugins/*.js',
+            'assets/js/_*.js'
+          ]
+        }
+      }
+    },
+    version: {
+      options: {
+        file: 'lib/scripts.php',
+        css: 'assets/css/main.min.css',
+        cssHandle: 'roots_main',
+        js: 'assets/js/scripts.min.js',
+        jsHandle: 'roots_scripts'
+      }
+    },
+    watch: {
+      sass: {
+        files: [
+          'assets/sass/*.scss',
+          'assets/sass/bootstrap/*.scss'
+        ],
+        tasks: ['sass', 'version']
+      },
+      js: {
+        files: [
+          '<%= jshint.all %>'
+        ],
+        tasks: ['jshint', 'uglify', 'version']
+      },
+      livereload: {
+        // Browser live reloading
+        // https://github.com/gruntjs/grunt-contrib-watch#live-reloading
+        options: {
+          livereload: false
+        },
+        files: [
+          'assets/css/main.min.css',
+          'assets/js/scripts.min.js',
+          'templates/*.php',
+          '*.php'
+        ]
+      }
+    },
+    clean: {
+      dist: [
+        'assets/css/main.min.css',
+        'assets/js/scripts.min.js'
+      ]
     }
   });
 
-  grunt.loadTasks('tasks');
+  // Load tasks
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-contrib-internal');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-wp-version');
 
-  grunt.registerTask('mkdir', grunt.file.mkdir);
-  grunt.registerTask('test', [
+  // Register tasks
+  grunt.registerTask('default', [
     'clean',
-    'mkdir:tmp',
     'sass',
-    'nodeunit',
-    'clean'
+    'uglify',
+    'version'
   ]);
-  grunt.registerTask('default', ['jshint', 'test', 'build-contrib']);
+  grunt.registerTask('dev', [
+    'watch'
+  ]);
+
 };
